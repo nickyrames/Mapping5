c3eb51d42af43172fa51634dc9500b2c89246f3f
Nicola Chigumbura <Nicola Chigumbura>
2017-02-06 16:57:09 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2chign70/mapping2/MapChooseActivity.java
@@ -0,0 +1,19 @@
+public class MapChooseActivity {
+    package com.example.map; // or whatever it is in your case
+
+    import android.app.Activity;
+    import android.os.Bundle;
+    import android.content.Intent;
+    import android.widget.Button;
+    import android.view.View;
+
+    public class MapChooseActivity extends Activity {
+
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.mca);
+        }
+    }
+
+}
