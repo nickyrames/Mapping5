0d7e6f9c577bab0c2b2f86c0499ac904a6b01734
nickyrames <facebook123w>
2017-02-20 15:46:35 +0000

    commited work from last week

--- app/src/main/java/com/example/a2chign70/mapping2/MapChooseActivity.java
+++ app/src/main/java/com/example/a2chign70/mapping2/MapChooseActivity.java
@@ -1,5 +1,5 @@
-public class MapChooseActivity {
-    package com.example.map; // or whatever it is in your case
+
+    package com.example.a2chign70.mapping2; // or whatever it is in your case
 
     import android.app.Activity;
     import android.os.Bundle;
@@ -7,13 +7,32 @@ public class MapChooseActivity {
     import android.widget.Button;
     import android.view.View;
 
-    public class MapChooseActivity extends Activity {
+    public class MapChooseActivity extends Activity implements View.OnClickListener {
 
         public void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.mca);
+            Button regular = (Button)findViewById(R.id.btnRegular);
+            regular.setOnClickListener(this);
+            Button cyclemap = (Button)findViewById(R.id.btnCyclemap);
+            cyclemap.setOnClickListener(this);
+        }
+
+        public void onClick(View v)
+        {
+            Intent intent = new Intent();
+            Bundle bundle=new Bundle();
+            boolean cyclemap=false;
+            if (v.getId()==R.id.btnCyclemap)
+            {
+                cyclemap=true;
+            }
+            bundle.putBoolean("com.example.cyclemap",cyclemap);
+            intent.putExtras(bundle);
+            setResult(RESULT_OK,intent);
+            finish();
         }
     }
 
-}
+
